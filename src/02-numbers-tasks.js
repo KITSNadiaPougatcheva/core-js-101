73d93d59c7d147f98c248b074d7a1c81faebb578
Nadia Pougatcheva <nadia_pougatcheva@epam.com>
2020-07-24 22:48:15 +0300

    fix: some number functions

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -131,8 +131,9 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  const v = value.toString();
+  return v.slice(-1);
 }
 
 
@@ -147,8 +148,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number.parseFloat(value);
 }
 
 /**
@@ -164,8 +165,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelipidedDiagonal(a, b, c) {
+  return Math.sqrt(a * a + b * b + c ** 2);
 }
 
 /**
@@ -185,8 +186,8 @@ function getParallelipidedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return Math.round(num / (10 ** pow)) * (10 ** pow);
 }
 
 /**
@@ -206,8 +207,15 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  if ([0, 2].includes(n)) return true;
+  if (n % 2 === 0) return false;
+
+  const x = Math.floor(n / 2);
+  for (let i = x; i > 2; i -= 1) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
@@ -225,8 +233,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return (!Number.isNaN(value) && Number(value)) || def;
 }
 
 module.exports = {
